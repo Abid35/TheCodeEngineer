db00cd809c36f6c01e3fe5c429c18262e7888fea
unknown <syedabidalishah483@gmail.com>
2025-08-05 22:44:36 +0500

    Initial commit

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,21 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+            Hi, I'm Engineer Abid Ali Shah — a Computer Engineer and Full Stack Developer with hands-on expertise in Laravel, the MERN stack (MongoDB, Express.js, React, Node.js), and WordPress. I specialize in crafting robust backend systems, seamless frontend interfaces, and fully customized CMS solutions.
+          </p>
+          <p className="hero--section-description">
+            From building dynamic business dashboards and financial systems in Laravel, to creating responsive React applications and custom WordPress themes, I deliver end-to-end web solutions that are fast, scalable, and tailored to client needs.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
