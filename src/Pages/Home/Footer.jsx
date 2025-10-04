81bde79817c8273de2cf83758b5c5d84b33737d4
Engineer Abid <90345453+Abid35@users.noreply.github.com>
2025-10-04 15:46:26 +0500

    Update Footer.jsx

--- src/Pages/Home/Footer.jsx
+++ src/Pages/Home/Footer.jsx
@@ -146,7 +146,7 @@ function Footer() {
             </li>
             <li>
               <a
-                href="https://www.linkedin.com/"
+                href="https://www.linkedin.com/in/abid-ali-shah-919880219"
                 className="navbar--content"
                 target="_blank"
                 rel="noreferrer"
